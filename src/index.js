cb16e9eb1ed025d32acacc6f86761b8461dcbc10
brychanmd <bmorrisdafydd@gmail.com>
2022-07-23 14:16:29 +0100

    listview class done

--- src/index.js
+++ src/index.js
@@ -10,18 +10,21 @@ import { testButton } from '@/js/example';
 // Styles.
 import '@/styles/index.scss';
 import Task from './js/task';
+import { addDays } from 'date-fns';
 
 let number = 0;
 
 const taskList = new TaskList();
+const listView = new ListView();
 
 const logTasks = () => {
   console.log(taskList.tasks);
 };
 
 const addTask = () => {
+  let date = addDays(Date.now(), number);
   number++;
-  const task = new Task(`Task number ${number}`, 'random-date-2022');
+  const task = new Task(`Task number ${number}`, date);
   taskList.addTask(task);
 };
 
@@ -29,12 +32,34 @@ const removeTask = () => {
   taskList.removeTask(taskList.tasks[0]);
 };
 
-const logger = testButton('Log tasks', ['tester'], logTasks);
-const addTaskBtn = testButton('Add task', [], addTask);
-const removeTaskBtn = testButton('Remove task', [], removeTask);
+const sortTasksAsc = () => {
+  listView.sortTasks(taskList.tasks, 'asc');
+};
+
+const sortTasksDesc = () => {
+  listView.sortTasks(taskList.tasks, 'desc');
+};
+
+const filterTasksToday = () => {
+  taskList.tasks = listView.filterTasks(taskList.tasks, 'today');
+};
+
+const filterTasksUpcoming = () => {
+  taskList.tasks = listView.filterTasks(taskList.tasks, 'upcoming');
+};
+
+const buttons = [
+  testButton('Log tasks', ['tester'], logTasks),
+  testButton('Add task', [], addTask),
+  testButton('Remove task', [], removeTask),
+  testButton('Sort asc task', [], sortTasksAsc),
+  testButton('Sort desc task', [], sortTasksDesc),
+  testButton('filter today', [], filterTasksToday),
+  testButton('filter upcoming', [], filterTasksUpcoming),
+];
 
 const heading = document.createElement('h1');
 heading.textContent = 'Testing the page title';
 
 const app = document.querySelector('#root');
-app.append(heading, logger, addTaskBtn, removeTaskBtn);
+app.append(heading, ...buttons);
--- src/js/listView.js
+++ src/js/listView.js
@@ -1,20 +1,48 @@
+import { compareAsc, compareDesc, format, isAfter, isBefore, endOfToday } from 'date-fns';
+import Task from './task';
+
 export default class ListView {
   /**
    *
-   * @param {string} date
+   * @param {Array<Task>} tasks
+   * @param {string} method
    */
-  filterTasks(date) {
-    switch (date) {
-      case 'today':
-        // do something
-        break;
+  filterTasks(tasks, method) {
+    switch (method) {
+      case 'upcoming':
+        return tasks.filter((task) => {
+          return isAfter(task.date, Date.now());
+        });
 
-      case 'week':
-        // do something
-        break;
+      case 'today':
+        return tasks.filter((task) => {
+          return isBefore(task.date, endOfToday());
+        });
 
       default:
-        break;
+        throw 'Invalid filter method';
+    }
+  }
+
+  /**
+   *
+   * @param {Array<Task>} tasks
+   * @param {string} method
+   */
+  sortTasks(tasks, method) {
+    switch (method) {
+      case 'asc':
+        return tasks.sort((a, b) => {
+          return compareAsc(a.date, b.date);
+        });
+
+      case 'desc':
+        return tasks.sort((a, b) => {
+          return compareDesc(a.date, b.date);
+        });
+
+      default:
+        throw 'Invalid sort method';
     }
   }
 }
